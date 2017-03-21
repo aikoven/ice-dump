88b67be25638254101dfa4a45b0104ef86bfbb4a
Daniel Lytkin <gaelco.salmon@gmail.com>
2017-03-21 20:51:50 +0700

    Add TypeScript typings

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+import {Ice} from "ice";
+
+export function objectToBuffer(object: Ice.Object,
+                               communicator: Ice.Communicator): Buffer;
+
+export function bufferToObject(buffer: Buffer,
+                               communicator: Ice.Communicator): Ice.Object;
