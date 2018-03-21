36f0fcfcc6ee88883e6d21740baaa1bf7e77761a
Daniel Lytkin <gaelco.salmon@gmail.com>
2018-03-21 12:40:27 +0700

    Update typings

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,21 @@
-import {Ice} from "ice";
+import {Ice} from 'ice';
 
-export function valueToBuffer(object: Ice.Value): Uint8Array;
+export function valueToBuffer(value: Ice.Value): Uint8Array;
 
 export function bufferToValue<V extends Ice.Value>(buffer: Uint8Array): V;
+
+export type IcePrimitive = string | number | boolean;
+export interface IceSimpleDictionary
+  extends Map<IcePrimitive, IcePrimitive | IceValue> {}
+export interface IceComplexDictionary
+  extends Ice.HashMap<Ice.HashMapKey, IcePrimitive | IceValue> {}
+export type IceDictionary = IceSimpleDictionary | IceComplexDictionary;
+export interface IceSequence extends Array<IcePrimitive | IceValue> {}
+
+export type IceValue = Ice.Value | Ice.Struct | IceDictionary | IceSequence;
+
+export function iceToBuffer(value: IceValue, type: string): Uint8Array;
+export function bufferToIce<V extends IceValue>(
+  buffer: Uint8Array,
+  type: string,
+): V;
